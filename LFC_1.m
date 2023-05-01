5e61c0cdb892a967cf5fe1100ac5f93ee62d1d7c
JMFBr <julia.mfb98@gmail.com>
2023-05-01 11:49:30 +0200

    2

--- LFC_1.m
+++ LFC_1.m
@@ -1,9 +1,9 @@
 clear;clc;close all
 %%
 
-N_0  = 4;  % #Planes
-N_s0 = 3; % #Sats/planes
-N_c  = 2; % Phasing parameter
+N_0  = 4; % #Planes
+N_s0 = 1; % #Sats/planes
+N_c  = 4; % Phasing parameter
 
 L = [N_0, 0;
     N_c, N_s0]; % Lattice matrix
@@ -46,4 +46,48 @@ scatter(C(:,:,2), C(:,:,1))
 grid on
 ylabel('\Omega (rad)')
 xlabel('M (rad)')
+
+%% OEs
+
+mu = 3.986e14; % [m3/s2], Earth standard gravitational parameter
+RE = 6371e3;   % [m], Earth Radius
+h  = 580e3;    % [m], Altitude
+
+a  = RE + h;
+e  = 0;
+i  = 72*pi/180; % [rad], Inclination
+om = 0*pi/180;  % [rad], argument of the perigee
+
+OM = C(:,:,1); % [rad], random RAAN value from the LFC computation
+M  = C(:,:,2); % [rad], random Mean Anomaly value from the LFC computation
+th  = M; % Mean anomaly = to Eccentric anomaly = to true anomaly for e=0
+
+
+
+th1 = 0;         % [rad]
+th2 = 2*pi;      % [rad]
+stepTh = pi/180; % [rad]
+
+Terra_3D
+for k = 1:size(th,1)
+    for l = 1:size(th,2)
+
+        [r,v] = kep2car(a, e, i, OM(k,l), om, th(k,l), mu);
+        scatter3(r(1)/1000, r(2)/1000, r(3)/1000, 'filled', 'LineWidth',1.5)
+
+        kepEl = [a, e, i, OM(k,l), om, th(k,l)];
+        [X, Y, Z] = plotOrbit(kepEl, mu, th1, th2, stepTh);
+        plot3(X./1000, Y./1000, Z./1000, 'Color','red')
+
+    end
+
+end
+
+
+
+
+
+
+
+
 
