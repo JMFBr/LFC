ad70b8e3cb827992bf868b3e1fd4ab83b6328da5
JMFBr <julia.mfb98@gmail.com>
2023-05-22 13:29:14 +0200

    - Constellation to eci - Targets to ecef

--- pruebasrapidas.py
+++ pruebasrapidas.py
@@ -54,107 +54,15 @@ om = 0 * np.pi / 180  # [rad], argument of the perigee
 
 (C, Omega, M, Omega_m, M_m) = LFC(N_0, N_s0, N_c)
 
-# Create constellation matrix
-const_matrix = np.ones((N_TS, 4))
-const_matrix[:, 0] = a  # [m]
-const_matrix[:, 1] = e
-const_matrix[:, 2] = inc  # [rad]
-const_matrix[:, 3] = om  # [rad]
-const_matrix = np.c_[const_matrix, Omega, M]  # Constellation matrix: (Nts x 6 OEs)
+
 
 
 ## CONSTELLATION TO ECI:
 
-def kep2eci(const_m_OE, t, T):  # R modified for matrices
-    """
-    Converts Keplerian orbital elements to Earth-centered inertial coordinates.
-    Parameters:
-    -----------
-    const_m_OE : constellation matrix with keplerian elements, array_like (N_TS x 6)
-        - ROWS: Orbital elements in the following order: semi-major axis (a) [m], eccentricity (e),
-        inclination (i) [rad], argument of peri-apsis (omega) [rad], right ascension of ascending node (Omega) [rad],
-        mean anomaly (M) [rad].
-        - COLUMNS: Satellites in the constellation
-
-    Returns:
-    --------
-    const_m_ECI : constellation matrix in ECI coordinates, array_like (N_TS x 6)
-        - ROWS: x_eci (x3), Earth-centered inertial coordinates in meters: x, y, z (m).
-                v_eci (x3), Earth-centered inertial velocity coordinates: vx, vy, vz (m/s).
-        - COLUMNS: Satellites in the constellation
-    """
-    # Define Keplerian orbital elements
-    a_v = const_m_OE[:, 0]
-    e_v = const_m_OE[:, 1]
-    i_v = const_m_OE[:, 2]
-    omega_v = const_m_OE[:, 3]
-    Omega_v = const_m_OE[:, 4]
-
-    # Initial position
-    M0_v = const_m_OE[:, 5]  # Initial mean anomaly vector
-    t0_v = M0_v*T/(2*np.pi)  # [s], Time from peri-apsis that corresponds to the initial true anomaly vector
-
-    # Current position
-    M_v = 2 * np.pi * (t0_v + t) / T  # Mean anomaly at current position
-
-    # Calculate eccentric anomaly
-    E = M_v
-    nu = np.zeros(N_TS)
-    for j in range(len(E)):  # Compute E using Newton's method for each satellite in teh constellation
-        while True:
-            E_new = E[j] + (M_v[j] - E[j] + e_v[j] * np.sin(E[j])) / (1 - e_v[j] * np.cos(E[j]))
-            if abs(E_new - E[j]) < 1e-8:
-                break
-            E[j] = E_new
-
-        if E[j] < 0:
-            E[j] += 2 * np.pi
-
-        # Calculate true anomaly corresponding to the current time t
-        nu[j] = 2 * np.arctan(np.sqrt((1 + e_v[j]) / (1 - e_v[j])) * np.tan(E[j] / 2))
-        if nu[j] < 0:
-            nu[j] += 2 * np.pi
-
-    # Calculate semi-latus rectum and mean motion
-    p = a_v * (1 - e_v ** 2)
-    # Calculate distance from Earth to satellite
-    r = p / (1 + e_v * np.cos(nu))
-
-    # Calculate position and velocity in peri-focal coordinates
-
-    x = np.array([r * np.cos(nu)])
-    y = np.array([r * np.sin(nu)])
-    z = np.zeros((N_TS, 1))
-    r_pqw = np.concatenate((x.T, y.T, z), axis=1)  # [m], Matrix: (Position vectors in peri-focal x N_TS)
-
-    vx = np.array([-np.sin(nu)]) * np.sqrt(mu / p)
-    vy = np.array([e + np.cos(nu)]) * np.sqrt(mu / p)
-    vz = np.zeros((N_TS, 1))
-    v_pqw = np.concatenate((vx.T, vy.T, vz), axis=1)  # [m/s], Matrix: (Velocity vectors in peri-focal x N_TS)
-
-    # Transformation matrix from peri-focal to geocentric equatorial coordinates. Dimensions: (3 x 3 x N_TS)
-    R_pqw_to_eci = np.array([
-        [np.cos(Omega_v) * np.cos(omega_v) - np.sin(Omega_v) * np.sin(omega_v) * np.cos(i_v),
-         -np.cos(Omega_v) * np.sin(omega_v) - np.sin(Omega_v) * np.cos(omega_v) * np.cos(i_v), np.sin(Omega_v) * np.sin(i_v)],
-        [np.sin(Omega_v) * np.cos(omega_v) + np.cos(Omega_v) * np.sin(omega_v) * np.cos(i_v),
-         -np.sin(Omega_v) * np.sin(omega_v) + np.cos(Omega_v) * np.cos(omega_v) * np.cos(i_v), -np.cos(Omega_v) * np.sin(i_v)],
-        [np.sin(i_v) * np.sin(omega_v), np.sin(i_v) * np.cos(omega_v), np.cos(i_v)]
-    ])
-
-    # Convert
-    x_eci = np.zeros((N_TS, 3))
-    v_eci = np.zeros((N_TS, 3))
-    for j in range(N_TS):
-        x_eci[j, :] = np.dot(R_pqw_to_eci[:, :, j], r_pqw[j, :])
-        v_eci[j, :] = np.dot(R_pqw_to_eci[:, :, j], v_pqw[j, :])
-
-    return x_eci, v_eci
 
 
-t = 0
-T = 2*np.pi*np.sqrt(a ** 3 / mu)  # [s], Orbital period
 
-X_eci, V_eci = kep2eci(const_matrix, t, T)
+
 
 
 
